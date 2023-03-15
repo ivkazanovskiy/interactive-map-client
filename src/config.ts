83cf57c88de4a1aa0165321b0123e29d54845de2
ivkazanovskiy <ivkazanovskiy@gmail.com>
2023-03-16 00:25:16 +0300

    feat: socket env

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const config = {
-  backendUrl: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000",
+  backendUrl: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/v1",
+  socketUrl: import.meta.env.VITE_SOCKET_URL || "http://localhost:3000",
 };
